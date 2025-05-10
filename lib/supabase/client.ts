a3e25cc6bfbf3a4884085b4a72671959478c0ba7
gunavarma <gunavarma0000@gmail.com>
2025-05-10 19:04:54 +0530

    name changed

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,6 +3,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace with your actual Supabase credentials
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
